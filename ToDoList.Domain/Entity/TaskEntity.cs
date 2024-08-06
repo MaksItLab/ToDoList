a365244bc224c95c89ebfe83dfddf003d5c487bc
MaksItLab <maksim.ardintsev@mail.ru>
2024-08-06 16:07:43 +0300

    Добавьте файлы проекта.

---
+++ ToDoList.Domain/Entity/TaskEntity.cs
@@ -0,0 +1,27 @@
+﻿using ToDoList.Domain.Enum;
+
+namespace ToDoList.Domain.Entity
+{
+    /// <summary>
+    /// Сущность задачи
+    /// </summary>
+    public class TaskEntity
+    {
+        /// <summary>
+        /// Уникальный идентификатор задачи 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Название задачи
+        /// </summary>
+        public string Name { get; set; }
+        /// <summary>
+        /// Описание задачи
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// Приоритет задачи
+        /// </summary>
+        public Priority Priority { get; set; }
+    }
+}
